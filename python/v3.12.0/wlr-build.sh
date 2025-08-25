846f263b80ad362c4c98ed6938eb6be8de94bb5b
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-08-25 16:29:30 +0545

    feat(python): add python-dateutil to custom packages

--- python/v3.12.0/wlr-build.sh
+++ python/v3.12.0/wlr-build.sh
@@ -62,7 +62,7 @@ fi
 export MAKE_TARGETS='python.wasm wasm_stdlib'
 
 logStatus "Installing custom packages into CPython Lib..."
-python3 -m pip install --target "${WLR_SOURCE_PATH}/Lib" requests || exit 1
+python3 -m pip install --target "${WLR_SOURCE_PATH}/Lib" requests python-dateutil || exit 1
 
 logStatus "Building '${MAKE_TARGETS}'... "
 make -j ${MAKE_TARGETS} || exit 1
